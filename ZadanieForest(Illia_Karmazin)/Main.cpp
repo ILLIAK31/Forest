3b4ecce7df696fad91cacaaf83c6b16bea41e087
Illia <karma@MYPC>
2023-03-23 20:14:29 +0100

    -

--- ZadanieForest(Illia_Karmazin)/Main.cpp
+++ ZadanieForest(Illia_Karmazin)/Main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+
 using namespace std;
+
+#include "Tree.hpp"
 
 int main()
 {
+	int menu_num;
+	do
+	{
+		cout << "Menu :" << endl << "1. Add tree to forest" << endl << "Enter number : ";
+		cin >> menu_num;
+		switch (menu_num)
+		{
+		case 1 :
+
+		default:
+			cout << "Wrong number";
+			break;
+		}
+
+	}while(true);
     return 0;
 }
---
+++ ZadanieForest(Illia_Karmazin)/Tree.hpp
@@ -0,0 +1,11 @@
+class Tree
+{
+public:
+	Tree();
+	~Tree();
+
+private:
+	string color;
+	char symbol;
+	int height;
+};
