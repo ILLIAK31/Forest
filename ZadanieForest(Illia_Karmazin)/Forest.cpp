09826872d58ec3429c23c58b7a6a895ce28c453d
Illia <karma@MYPC>
2023-03-25 13:42:59 +0100

    .

--- ZadanieForest(Illia_Karmazin)/Forest.cpp
+++ ZadanieForest(Illia_Karmazin)/Forest.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 Forest::Forest(int Size , vector<vector<int>>Forest)
 {
+	size_forest = Size;
 	for (int i = 0; i < Size; ++i)
 	{
 		for (int j = 0; j < 2 * Size; ++j)
@@ -20,14 +21,14 @@ Forest::Forest(int Size , vector<vector<int>>Forest)
 
 void Forest::AddTree(Tree* obj3)
 {
-	if (((obj3->height) + (obj3->y)) > size_forest)
+	if (((obj3->height) + ((obj3->y)-1)) > size_forest)
 	{
-		cout << "Tree to big for this forest" << endl;
+		cout << "Tree too big for this forest" << size_forest << endl;
 		return;
 	}
-	if (((obj3->x) + ((2 * (obj3->height)) - 1)) > size_forest)
+	if ((((obj3->x)-1) + ((2 * (obj3->height)) - 1)) > (2*size_forest))
 	{
-		cout << "Tree to big for this forest" << endl;
+		cout << "Tree too big for this forest" << endl;
 		return;
 	}
 	wektor.push_back(obj3);
